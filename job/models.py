5ef817fece486a0980971073e767a84b8d49bfdc
egAhmed <a7meda00@gmail.com>
2020-12-22 12:35:46 +0200

    add job templates and model upload img

--- job/models.py
+++ job/models.py
@@ -9,6 +9,11 @@ JOB_TYPE = (
     ('Part Time','Part Time'),
 )
 
+def image_upload(instance,filename):
+    imagename , extension = filename.split(".")
+    return "jobs/%s.%s"%(instance.id,extension)
+    
+
 class Job(models.Model):  # table 
     owner = models.ForeignKey(User, related_name='job_owner', on_delete=models.CASCADE)
     title = models.CharField(max_length=100)  # column
@@ -20,7 +25,7 @@ class Job(models.Model):  # table
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1) 
     category = models.ForeignKey('Category',on_delete=models.CASCADE)
-    #image = models.ImageField(upload_to=image_upload)
+    image = models.ImageField(upload_to=image_upload)
 
     slug = models.SlugField(blank=True, null=True)
 
